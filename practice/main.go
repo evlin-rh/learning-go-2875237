24fb991cca4b6a53e221374e5de76f1060eb4ed4
evlin-rh <evlin@redhat.com>
2025-03-25 12:21:36 +0000

    DONE: Pointers

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,17 @@ import (
 )
 
 func main() {
-	fmt.Println("Pointers")
+
+	anInt := 42
+	var p = &anInt //* = shows its pointer not value
+	fmt.Println("Value of p: ", *p)
+
+	value1 := 41.23
+	pointer1 := &value1
+	fmt.Println("Value1: ", *pointer1)
+
+	*pointer1 = *pointer1 / 31
+	fmt.Println("Pointer1: ", *pointer1)
+	fmt.Println("Value1: ", value1)
+
 }
