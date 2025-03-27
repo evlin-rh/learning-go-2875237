b5ea71912fb2eae185a79d6ad60817c806d1ea8f
evlin-rh <evlin@redhat.com>
2025-03-27 09:39:10 +0000

    DONE: READ / WRITE File

--- practice/main.go
+++ practice/main.go
@@ -2,10 +2,33 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 )
 
 const url = "http://services.explorecalifornia.org/json/tours.php"
 
 func main() {
 	fmt.Println("Network requests")
+
+	content := "Hello From Go"
+	file, err := os.Create("./fromString1.txt")
+	checkError(err)
+	length, err := io.WriteString(file, content)
+	checkError(err)
+	fmt.Printf("Wrote a file with %v characters \n", length)
+	defer file.Close()
+	defer readFile("./fromString.txt")
+}
+
+func readFile(fileName string) {
+	data, err := os.ReadFile(fileName)
+	checkError(err)
+	fmt.Println(string(data))
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
