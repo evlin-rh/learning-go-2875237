8567ea12ec8dd690b2b2665b6d6632126b250625
evlin-rh <evlin@redhat.com>
2025-03-27 09:15:39 +0000

    DONE: Functions and params

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,31 @@ import (
 )
 
 func main() {
-	fmt.Println("Functions")
+	sum := addValues(10, 20)
+	fmt.Println("The sum is: ", sum)
+	sum1 := addValue2(10, 10)
+	fmt.Println("The sum is: ", sum1)
+	doSomething()
+	multiSum, multiCount := addAllValue(1, 2, 3, 4, 5)
+	fmt.Println("Multivalue: ", multiSum, "MultiCount: ", multiCount)
+}
+
+func doSomething() {
+	fmt.Println("Do sth")
+}
+
+func addValues(value1 int, value2 int) int {
+	return value1 + value2
+}
+
+func addValue2(value1, value2 int) int {
+	return value1 + value2
+}
+
+func addAllValue(values ...int) (int, int) {
+	total := 0
+	for _, value := range values {
+		total += value
+	}
+	return total, len(values)
 }
